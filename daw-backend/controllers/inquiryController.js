8e8312c14f9cd5dad7c19fac2fb13a80c3cae0f3
Calvin-braus <75593027+calvnvn@users.noreply.github.com>
2026-03-17 11:03:52 +0700

    -

--- daw-backend/controllers/inquiryController.js
+++ daw-backend/controllers/inquiryController.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  tls: { rejectUnauthorized: false }, // Penting untuk jaringan kantor
+  tls: { rejectUnauthorized: false },
 });
 
 exports.submitInquiry = async (req, res) => {
